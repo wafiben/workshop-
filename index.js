cb8ed847c56d46f5c7ef89c836f4f376ac4d6adf
wafiben <wafibenjeddou@gmail.com>
2021-11-28 11:31:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+/* let x=5;
+let username='mabrouka';
+for(let i=0;i<2;i++){
+  console.log(i)
+  let username='firas';
+}
+ for (let i=0;i<3;i++){
+  console.log(i)
+  console.log(username)
+
+}  */
+ /* let table=[0,1,2,3,12]; */
+
+/* for(let i=0;i<table.length;i++){
+  table[i]=table[i]+1
+} 
+ */
+/* array=table;
+console.log(array); */
+/* const array=table.map((elt)=>{
+  return elt+1
+});
+console.log(array) */
+/* let array=[1,2,5,8];
+let table=array.filter((elt)=>elt!=5)
+console.log(table) */
+/* const person={lastName:'jrad',firstName:'mabrouka',age:26}
+/* console.log( typeof person.age); */
+/* console.log(person.lastName) */// dot notation 
+/* const {lastName,firstName,age}=person;//destructring
+console.log(lastName); */ 
+/* const find=(str)=>{
+  let count=0;
+  let arrayOfVowels=['a','e','u','i','o'];
+    str.split('').map((elt)=>{
+     if(elt.match(/[aeiou]/gi)){
+      count++
+    }      
+   })  
+  
+  return count 
+}
+find('friras') */
+//spreading
+/* let array=[5,6,7]
+let x=5
+array.push(x)
+console.log(array)
+let newArray=array */
+/* let newArray=[...aray,x]; */
+
+
+
+
+
